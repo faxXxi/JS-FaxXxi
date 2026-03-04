b2b658640b5bec095353d462221a9fd6649c3ed3
faxXxi <faaiz90.dev@gmail.com>
2026-03-04 07:37:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJS, not Browser
+
+console.log(3 
+    + 3) // code readability must be high
+
+console.log("faaiz")
+
+let name = "Faaiz"
+let age = 25
+let isLoggedIn = false
+
+let state;
+
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
+
+console.log(typeof undefined);
