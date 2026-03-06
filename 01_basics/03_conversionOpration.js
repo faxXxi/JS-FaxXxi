3fdfa89d787160099e39b38e4abbdfb6fb8c35d9
faxXxi <faaiz90.dev@gmail.com>
2026-03-06 07:19:48 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,28 @@
+let score = "Faaiz"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33 number
+// 33abc => NaN
+//true => 1 false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// Faaiz => true
+
+let someNumber = 5
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
